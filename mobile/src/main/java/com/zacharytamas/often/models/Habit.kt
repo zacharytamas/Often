17c3ce033cd69f218b1da084a72bab13ccf920a9
Zachary Jones <zacharytamas@gmail.com>
2015-10-08 13:16:07 -0400

    These are implicitly public so no point in explicitly stating it.

--- mobile/src/main/java/com/zacharytamas/often/models/Habit.kt
+++ mobile/src/main/java/com/zacharytamas/often/models/Habit.kt
@@ -8,18 +8,18 @@ import java.util.*
  * Created by zacharytamas on 10/8/15.
  */
 public class Habit: RealmObject() {
-    @PrimaryKey public var id: Int = 0
-    public var title: String = ""
-    public var required: Boolean = true
+    @PrimaryKey var id: Int = 0
+    var title: String = ""
+    var required: Boolean = true
 
-    public var repeatType: Byte = 0
-    public var repeatUnit: Int = 0
-    public var repeatScalar: Int = 0
-    public var repeatWeekdays: Byte = 0
+    var repeatType: Byte = 0
+    var repeatUnit: Int = 0
+    var repeatScalar: Int = 0
+    var repeatWeekdays: Byte = 0
 
-    public var createdAt: Date? = null
-    public var availableAt: Date? = null
-    public var lastCompletedAt: Date? = null
-    public var dueAtSpecificTime: Boolean = false
-    public var dueAt: Date? = null
+    var createdAt: Date? = null
+    var availableAt: Date? = null
+    var lastCompletedAt: Date? = null
+    var dueAtSpecificTime: Boolean = false
+    var dueAt: Date? = null
 }
