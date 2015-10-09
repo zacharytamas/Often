6e3aff997ce412374bf50d5ca77fd4cd8cb7c634
Zachary Jones <zacharytamas@gmail.com>
2015-10-09 12:52:16 -0400

    Support multidex the proper way.

---
+++ mobile/src/main/java/com/zacharytamas/often/Application.kt
@@ -0,0 +1,10 @@
+package com.zacharytamas.often
+
+import android.support.multidex.MultiDexApplication
+
+/**
+ * Created by zacharytamas on 10/9/15.
+ */
+class Application : MultiDexApplication() {
+
+}
