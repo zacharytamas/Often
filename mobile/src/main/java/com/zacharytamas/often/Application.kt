ef4b5fa494ca1965441f59243ba69314a9ff8f82
Zachary Jones <zacharytamas@gmail.com>
2015-10-24 15:14:33 -0400

    Configure Sugar.
    Not so sure how I'm going to manage SugarApp and MultidexApplication at the same time

--- mobile/src/main/java/com/zacharytamas/often/Application.kt
+++ mobile/src/main/java/com/zacharytamas/often/Application.kt
@@ -1,10 +1,11 @@
 package com.zacharytamas.often
 
 import android.support.multidex.MultiDexApplication
+import com.orm.SugarApp
 
 /**
  * Created by zacharytamas on 10/9/15.
  */
-class Application : MultiDexApplication() {
+class Application : SugarApp() {
 
 }
