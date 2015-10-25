de77007d1f05648927bee606d7ada2f1aed4c331
Zachary Jones <zacharytamas@gmail.com>
2015-10-25 19:03:45 -0400

    HabitManager.kt -> HabitManager.java

--- mobile/src/main/java/com/zacharytamas/often/models/RepeatType.java
+++ mobile/src/main/java/com/zacharytamas/often/models/RepeatType.java
@@ -3,7 +3,7 @@ package com.zacharytamas.often.models;
 /**
  * Created by zacharytamas on 10/25/15.
  */
-public abstract class RepeatType {
+public class RepeatType {
     public static final byte PERIODICAL = 1;
     public static final byte WEEKLY = 2;
 }
